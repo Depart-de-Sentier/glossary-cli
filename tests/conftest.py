0dbc40fbd5c199c389543967f5e75e487b633e46
João Gonçalves <jsvgoncalves@gmail.com>
2024-05-19 23:53:56 +0200

    Set locale for github's ubuntu
    * By default ubuntu sets locale to `C`, which won't work to get the
      language.
    
    Signed-off-by: João Gonçalves <jsvgoncalves@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
 """Fixtures for sentier_glossary"""
+import pytest
+import locale
 
-# import pytest
+@pytest.fixture(scope="session", autouse=True)
+def set_locale():
+    try:
+        locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+    except locale.Error:
+        pytest.skip("locale not available")
