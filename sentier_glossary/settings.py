cc4f09f3cd2ed36075a5b4a6559ed2348d6aca5b
João Gonçalves <jsvgoncalves@gmail.com>
2024-05-20 14:08:28 +0200

    Add dot-env support
    * Allow settings to be read from `.env` file.
    
    Signed-off-by: João Gonçalves <jsvgoncalves@gmail.com>

--- sentier_glossary/settings.py
+++ sentier_glossary/settings.py
@@ -1,4 +1,7 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     base_url: str = "https://api.g.sentier.dev/"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
