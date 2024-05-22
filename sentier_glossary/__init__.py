99b7f2ccdae7be243417f70faef444e02a5c5540
Chris Mutel <cmutel@gmail.com>
2024-05-22 07:06:49 -0400

    Start with sentry error logging

--- sentier_glossary/__init__.py
+++ sentier_glossary/__init__.py
@@ -6,5 +6,10 @@ __all__ = (
 
 __version__ = "0.4.0"
 
+import sentry_sdk
+sentry_sdk.init(
+    dsn="https://9e46eb069bb559eed2b074ee8b20d19e@o4507277389004800.ingest.de.sentry.io/4507299672883280",
+    traces_sample_rate=1.0,
+)
 
 from .main import CommonSchemes, GlossaryAPI
